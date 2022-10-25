89f4429ccff36310f9c21cc3fb872a20596c338f
Charlie Bertrand <charlie.bertrand@live.com>
2022-10-25 15:37:35 +0200

    adding deletion feature

--- App.js
+++ App.js
@@ -5,16 +5,22 @@ import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-    const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
-    function addGoalHandler(enteredGoalText) {
-      // setCourseGoals([...courseGoals, enteredGoalText]);
-      setCourseGoals((currentCourseGoals) => [
-        ...currentCourseGoals,
-        { text: enteredGoalText, id: Math.random().toString() },
-      ]);
-    }
+  function addGoalHandler(enteredGoalText) {
+    // setCourseGoals([...courseGoals, enteredGoalText]);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
 
+  function deleteGoalHandler(id) {
+    console.log("DELETE");
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter( (goal) => goal.id !== id);
+    })
+  }
 
   return (
     <View style={styles.appContainer}>
@@ -24,7 +30,13 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+                id={itemData.item.id}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
