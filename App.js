a9359bae2b1c67755379faf19e33af7ef8b102d9
Charlie Bertrand <charlie.bertrand@live.com>
2022-10-25 11:12:24 +0200

    inital setup

--- App.js
+++ App.js
@@ -4,7 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Hello world !</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
+      <Text>Woops</Text>
       <StatusBar style="auto" />
     </View>
   );
