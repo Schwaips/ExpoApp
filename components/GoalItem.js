34345db66d70522a2d4901f4d887742ddc76ddb3
Charlie Bertrand <charlie.bertrand@live.com>
2022-10-25 15:45:31 +0200

    adding style for deletion

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,24 +2,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem  }
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
-};
+}
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
-    backgroundColor: "lightblue",
+    backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
   },
+  pressedItem: {
+    opacity: 0.5
+  }
 });
